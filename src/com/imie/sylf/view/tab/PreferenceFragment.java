097c8e1ce83b9b7614a30cc764ab93bfeafc8514
JMartelot <jeanmartelot@gmail.com>
2015-01-26 23:02:13 +0100

    Add slider

--- src/com/imie/sylf/view/tab/PreferenceFragment.java
+++ src/com/imie/sylf/view/tab/PreferenceFragment.java
@@ -2,32 +2,36 @@ package com.imie.sylf.view.tab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.imie.sylf.R;
+
 
 public class PreferenceFragment extends Fragment {
-	
-	public static final String EXTRA_TITLE = "title";
-	 
+    
+    private SliderLayout mDemoSlider;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        TextView txt = new TextView( inflater.getContext() );
-        txt.setGravity( Gravity.CENTER );
-        txt.setText( "Fragment" );
- 
-        if ( getArguments() != null && getArguments().containsKey( EXTRA_TITLE ) ) {
-            txt.setText( getArguments().getString( EXTRA_TITLE ) );
-        }
-        return txt;
-    }
- 
-    public static Bundle createBundle( String title ) {
-        Bundle bundle = new Bundle();
-        bundle.putString( EXTRA_TITLE, title );
-        return bundle;
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+        View view = inflater.inflate(R.layout.fragment_preferences, container, false);
+        
+        mDemoSlider = (SliderLayout)view.findViewById(R.id.slider);
+
+        TextSliderView demoSlider = new TextSliderView(this.getActivity());
+        demoSlider.description("Game of Thrones")
+                  .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+        mDemoSlider.addSlider(demoSlider);
+        
+        return view;
     }
 }
