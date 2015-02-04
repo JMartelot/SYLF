921902156a38524e41d2d51667dc6bb123329847
Quentin Andrieu <quentinandrieumail@gmail.com>
2015-02-04 22:00:24 +0100

    Ajout d'une lib + test dans profil

--- src/com/imie/sylf/HomeActivity.java
+++ src/com/imie/sylf/HomeActivity.java
@@ -6,22 +6,24 @@ import com.imie.sylf.view.tab.GenreFragment;
 import com.imie.sylf.view.tab.PreferenceFragment;
 import com.imie.sylf.view.tab.ProfilFragment;
 import com.imie.sylf.view.tab.RandomFragment;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class HomeActivity extends TabSwipeActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);     
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		getSupportActionBar().setDisplayShowHomeEnabled(false);
+		getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(false);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-                    
-        addTab( R.drawable.random, RandomFragment.class, null );
-        addTab( R.drawable.favorite, PreferenceFragment.class,null );
-        addTab( R.drawable.genre, GenreFragment.class, null ); 
-        addTab( R.drawable.profil, ProfilFragment.class, ProfilFragment.createBundle((String) getText(R.string.profil)) );
-    }
-    
+		addTab( R.drawable.random, RandomFragment.class, null );
+		addTab( R.drawable.favorite, PreferenceFragment.class,null );
+		addTab( R.drawable.genre, GenreFragment.class, null ); 
+		addTab( R.drawable.profil, ProfilFragment.class, ProfilFragment.createBundle((String) getText(R.string.profil)) );
+	}
+
 }
--- src/com/imie/sylf/view/tab/ProfilFragment.java
+++ src/com/imie/sylf/view/tab/ProfilFragment.java
@@ -1,11 +1,15 @@
 package com.imie.sylf.view.tab;
 
+import com.imie.sylf.R;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfilFragment extends Fragment {
@@ -18,6 +22,11 @@ public class ProfilFragment extends Fragment {
         TextView txt = new TextView( inflater.getContext() );
         txt.setGravity( Gravity.CENTER );
         txt.setText( "Fragment" );
+        
+		ImageView imageView = new ImageView(inflater.getContext());
+        
+		UrlImageViewHelper.setUrlDrawable(imageView, "http://example.com/image.png");
+
  
         if ( getArguments() != null && getArguments().containsKey( EXTRA_TITLE ) ) {
             txt.setText( getArguments().getString( EXTRA_TITLE ) );
