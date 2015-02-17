2069a11bfe93e080d3edd8668073d5cd13c64ac7
JMartelot <jeanmartelot@gmail.com>
2015-02-17 22:06:19 +0100

    Config UIL

--- src/com/imie/sylf/HomeActivity.java
+++ src/com/imie/sylf/HomeActivity.java
@@ -6,23 +6,44 @@ import com.imie.sylf.view.tab.GenreFragment;
 import com.imie.sylf.view.tab.PreferenceFragment;
 import com.imie.sylf.view.tab.ProfilFragment;
 import com.imie.sylf.view.tab.RandomFragment;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class HomeActivity extends TabSwipeActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		getSupportActionBar().setDisplayShowHomeEnabled(false);
-		getSupportActionBar().setDisplayShowTitleEnabled(false);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		addTab( R.drawable.random, RandomFragment.class, null );
-		addTab( R.drawable.favorite, PreferenceFragment.class,null );
-		addTab( R.drawable.genre, GenreFragment.class, null ); 
-		addTab( R.drawable.profil, ProfilFragment.class, ProfilFragment.createBundle((String) getText(R.string.profil)) );
-	}
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        
+        initUIL();
+        
+        addTab( R.drawable.random, RandomFragment.class, null );
+        addTab( R.drawable.favorite, PreferenceFragment.class,null );
+        addTab( R.drawable.genre, GenreFragment.class, null ); 
+        addTab( R.drawable.profil, ProfilFragment.class, ProfilFragment.createBundle((String) getText(R.string.profil)) );
+
+
+    }
+
+    private void initUIL(){
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+        .cacheInMemory(true)
+        .cacheOnDisk(true)
+        .build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+        .threadPoolSize(5)
+        .defaultDisplayImageOptions(defaultOptions)
+        .build();
+        ImageLoader.getInstance().init(config);
+    }
 
 }
