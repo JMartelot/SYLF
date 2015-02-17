b363bfeae9fdcfd2b363bf5796382cbb338f4a3c
JMartelot <jeanmartelot@gmail.com>
2015-02-17 10:52:14 +0100

    Preference Genre

--- src/com/imie/sylf/HomeActivity.java
+++ src/com/imie/sylf/HomeActivity.java
@@ -1,5 +1,10 @@
 package com.imie.sylf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.imie.sylf.entity.Genre;
+import com.imie.sylf.util.Parser;
 import com.imie.sylf.util.TabSwipeActivity;
 import com.imie.sylf.view.show.ShowListFragment;
 import com.imie.sylf.view.tab.GenreFragment;
@@ -9,8 +14,8 @@ import com.imie.sylf.view.tab.RandomFragment;
 
 import android.os.Bundle;
 
-public class HomeActivity extends TabSwipeActivity {
-
+public class HomeActivity extends TabSwipeActivity{
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);     
@@ -23,5 +28,4 @@ public class HomeActivity extends TabSwipeActivity {
         addTab( R.drawable.genre, GenreFragment.class, null ); 
         addTab( R.drawable.profil, ProfilFragment.class, null );
     }
-    
 }
--- src/com/imie/sylf/view/tab/ProfilFragment.java
+++ src/com/imie/sylf/view/tab/ProfilFragment.java
@@ -2,11 +2,20 @@ package com.imie.sylf.view.tab;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.imie.sylf.R;
+import com.imie.sylf.entity.Genre;
+import com.imie.sylf.util.Parser;
+import com.imie.sylf.util.WebServices;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,17 +24,23 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ProfilFragment extends Fragment {
+public class ProfilFragment extends Fragment implements Parser<Genre>  {
 
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ID = "id";
     private ArrayList<String> years = new ArrayList<String>();
-    private View view = null;
-    private RelativeLayout rl = null;
-    private Fragment fragment = null;
-    private CheckBox cb = null;
+    private List<Genre> genreList = new ArrayList<Genre>();
+    private View view;
+    private RelativeLayout rl_date;
+    private CheckBox cb;
+    private JSONArray genres;
+    private LinearLayout row;
 
     @Override
     public View onCreateView(
@@ -34,20 +49,24 @@ public class ProfilFragment extends Fragment {
             Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
-
+        
+        setRetainInstance(true);
+        
+        WebServices ws = new WebServices(this.getActivity());
+        ws.parser = this;
+        ws.execute("http://api.themoviedb.org/3/genre/tv/list?api_key=0d2d4cca633bc7bc04a564ac8266d3a1");
+        
         this.view = inflater.inflate(R.layout.fragment_profil, container, false);
 
         this.cb = (CheckBox) view.findViewById(R.id.doesnt_matter);
-        this.rl = (RelativeLayout) this.view.findViewById(R.id.date);
+        this.rl_date = (RelativeLayout) this.view.findViewById(R.id.date);
 
         if (!cb.isChecked()) {
             displayDate();
-            rl.setVisibility(View.VISIBLE);
+            rl_date.setVisibility(View.VISIBLE);
         }else{
-            rl.setVisibility(View.GONE);
+            rl_date.setVisibility(View.GONE);
         }
-        
-        fragment = this;
         
         cb.setOnClickListener(new OnClickListener() {
 
@@ -55,24 +74,13 @@ public class ProfilFragment extends Fragment {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 if(ProfilFragment.this.cb.isChecked()){
-                    ProfilFragment.this.rl.setVisibility(View.GONE);
+                    ProfilFragment.this.rl_date.setVisibility(View.GONE);
                 }else{
                     ProfilFragment.this.displayDate();
-                    ProfilFragment.this.rl.setVisibility(View.VISIBLE);                    
+                    ProfilFragment.this.rl_date.setVisibility(View.VISIBLE);                    
                 }
             }
         });
-
-//        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
-//                if(!isChecked){
-//                    ProfilFragment.this.rl.setVisibility(View.VISIBLE);
-//                    ProfilFragment.this.displayDate();
-//                }
-//            }
-//        });
 
         return view;
     }
@@ -97,5 +105,68 @@ public class ProfilFragment extends Fragment {
         spinner1.setAdapter(adapter);
         spinner2.setAdapter(adapter);
         spinner2.setSelection(adapter.getPosition(""+thisYear));
+    }
+
+    @Override
+    public void parseJSON(String stream) {
+        if (stream != null) {
+        try {
+            JSONObject jsonObj = new JSONObject(stream);
+             
+            // Getting JSON Array node
+             genres = jsonObj.getJSONArray("genres");
+
+            // looping through All Contacts
+            for (int i = 0; i < genres.length(); i++) {
+                JSONObject c = genres.getJSONObject(i);
+                Genre genre = new Genre();   
+                 
+                String name = c.getString(TAG_NAME);
+                String id = c.getString(TAG_ID);
+
+                genre.setTitle(name);
+                genre.setId(Integer.parseInt(id));
+
+                genreList.add(genre);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    } else {
+        Log.e("ServiceHandler", "Couldn't get any data from the url");
+    }
+    
+    listPopulate(genreList);
+        
+    }
+
+    @Override
+    public void listPopulate(List<Genre> liste) {
+        int i =0;
+
+        LinearLayout container = (LinearLayout) this.view.findViewById(R.id.genre);
+        
+        for (Genre genre : liste) {
+            CheckBox cb = new CheckBox(this.getActivity());
+            cb.setText(genre.getTitle());
+            cb.setId(genre.getId());
+            cb.setWidth(350);
+            
+            if (i%2 == 0) {
+                this.row = new LinearLayout(this.getActivity());
+                this.row.setOrientation(LinearLayout.HORIZONTAL);
+                this.row.addView(cb);
+            }else{
+                this.row.addView(cb);
+                container.addView(this.row);
+            }
+            i++;
+          }
+        }
+
+    @Override
+    public void entityPopulate(Genre entity) {
+        // TODO Auto-generated method stub
+        
     }
 }
